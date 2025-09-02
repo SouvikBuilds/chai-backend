3c772f538304bb8ac525f26396b622c854476f05
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2025-09-02 09:58:37 +0530

    User and Video Models With JWT Tokens

---
+++ chai-backend/src/models/video.models.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: [true, "Video File Is Required"],
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: [true, "Thumbnail File Is Required"],
+    },
+    title: {
+      type: String,
+      required: [true, "Title Is Required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description Required"],
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
